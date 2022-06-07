13ca6358bf7f29b9715ebdbce0e1fe7918f0d145
15682532888 <150273594@qq.com>
2022-06-07 19:53:45 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace Usb2X_FormDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 69);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Device";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 198);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(462, 229);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(498, 257);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 69);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Data from slave:";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(498, 358);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(168, 69);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "clear";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 23);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(306, 22);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(168, 69);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "StartCan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(498, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(168, 69);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "StopCan";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(498, 198);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(168, 23);
+            this.comboBox2.TabIndex = 13;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(495, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 15);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Change cmd:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 100);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(462, 62);
+            this.textBox2.TabIndex = 15;
+            this.textBox2.Text = "A5 5A 00 01 06";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(498, 97);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(168, 69);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "SendLeftData";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 15);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Usb Rx usage:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(425, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "KB/S";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(386, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 15);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 441);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Security.Cryptography;
+
+namespace Usb2X_FormDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static List<byte[]> listCmd = new List<byte[]>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listCmd.Add(cmd.cmd_SetCanBps);
+            listCmd.Add(cmd.cmd_TransmitStdFrame2Can);
+            listCmd.Add(cmd.cmd_SetCanRxFilter01);
+            listCmd.Add(cmd.cmd_StartCan);
+            listCmd.Add(cmd.cmd_StopCan);
+            listCmd.Add(cmd.cmd_Test);
+            listCmd.Add(cmd.cmd_KLine25msInitAndStart);
+            comboBox2.Items.Add("cmd_SetCanBps");
+            comboBox2.Items.Add("cmd_TransmitStdFrame2Can");
+            comboBox2.Items.Add("cmd_SetCanRxFilter01");
+            comboBox2.Items.Add("cmd_StartCan");
+            comboBox2.Items.Add("cmd_StopCan");
+            comboBox2.Items.Add("cmd_Test");
+            comboBox2.Items.Add("cmd_KLine25msInitAndStart");
+            comboBox2.SelectedIndex = 0;
+
+            updateComboBox();
+
+            Thread th1 = new Thread(Thread_RxHandle);
+            th1.Start();
+        }
+
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            updateComboBox();
+        }
+
+        private void updateComboBox()
+        {
+            comboBox1.Items.Clear();
+            foreach (string com in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                if (!comboBox1.Items.Contains(com))
+                {
+                    comboBox1.Items.Add(com);
+                }
+            }
+
+            if (0 < comboBox1.Items.Count)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        static Queue<byte[]> queue = new Queue<byte[]>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+                serialPort1.PortName = comboBox1.Text;
+                serialPort1.Open();
+                serialPort1.Write(cmd.cmd_Test, 0, cmd.cmd_Test.Length);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("端口打开失败！" + er.Message, "提示");
+            }
+        }
+
+        private void Thread_RxHandle()
+        {
+            while (true)
+            {
+                if (0 < queue.Count)
+                {
+                    for (int i = 0; i < queue.Count; i++)
+                    {
+                        usbFramesProcess(queue.Dequeue());
+                    }
+                }
+
+                Thread.Sleep(5);
+            }
+        }
+
+        private void usbFramesProcess(byte[] frames)
+        {
+            if (null != frames)
+            {
+                usbFrameLength += frames.Length;
+
+                int usbFrameCount = frames.Length / 64;
+                for (int i = 0; i < usbFrameCount; i++)
+                {
+                    byte[] frame = frames.Skip(i * 64).Take(64).ToArray();
+                    ListChanged(frame);
+                }
+            }
+        }
+
+        private delegate void SetText(byte[] data);
+        private void ListChanged(byte[] data)
+        {
+            if (this.textBox1.InvokeRequired)
+            {
+                SetText setText = ListChanged;
+                this.Invoke(setText, new object[] { data });
+            }
+            else
+            {
+                string s = "";
+                for (int i = 0; i < data.Length; i++)
+                {
+                    s += data[i].ToString("X") + " ";
+                }
+                this.textBox1.Text = s;
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int bytes = serialPort1.BytesToRead;
+            byte[] data = new byte[bytes];
+            serialPort1.Read(data, 0, bytes);
+            if (0 < bytes)
+            {
+                queue.Enqueue(data);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(cmd.cmd_StartCan, 0, cmd.cmd_StartCan.Length);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(cmd.cmd_StopCan, 0, cmd.cmd_StopCan.Length);
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(listCmd[comboBox2.SelectedIndex], 0, listCmd[comboBox2.SelectedIndex].Length);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                string s = textBox2.Text.Replace(" ", "");
+                byte[] cmdData = new byte[s.Length / 2];
+                for (int i = 0; i < s.Length; i += 2)
+                    cmdData[i / 2] = (byte)Convert.ToByte(s.Substring(i, 2), 16);
+                serialPort1.Write(cmdData, 0, cmdData.Length);
+            }
+        }
+
+        static int usbFrameLength = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label5.Text = (usbFrameLength / 1024).ToString();
+            usbFrameLength = 0;
+        }
+    }
+
+    class cmd
+    {
+        public static byte[] cmd_SetCanBps = new byte[7] { 0xa5, 0x5a, 0x00, 0x03, 0x01, 0x00, 0x7d };
+        public static byte[] cmd_TransmitStdFrame2Can = new byte[15] { 0xa5, 0x5a, 0x00, 0x0b, 0x00, 0x07, 0xfb, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11 };
+        public static byte[] cmd_SetCanRxFilter01 = new byte[13] { 0xa5, 0x5a, 0x00, 0x09, 0x02, 0x07, 0x72, 0x07, 0xfe, 0x07, 0xfd, 0x07, 0xfc };
+        public static byte[] cmd_StartCan = new byte[5] { 0xa5, 0x5a, 0x00, 0x01, 0x04 };
+        public static byte[] cmd_StopCan = new byte[5] { 0xa5, 0x5a, 0x00, 0x01, 0x05 };
+        public static byte[] cmd_Test = new byte[5] { 0xa5, 0x5a, 0x00, 0x01, 0x06 };
+        public static byte[] cmd_KLine25msInitAndStart = new byte[9] { 0xa5, 0x5a, 0x00, 0x05, 0x08, 0x81, 0x10, 0xF1, 0x81 };
+    }
+}
